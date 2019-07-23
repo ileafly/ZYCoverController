a285c19e7820abc6acc2be4b71d181a634e8df1a
lzy <luzhiyong@km.com>
2019-07-23 11:29:49 +0800

    support carthage

---
+++ Example/ZYCoverControllerFramework/ZYCoverControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  ZYCoverControllerFramework.h
+//  ZYCoverControllerFramework
+//
+//  Created by luzhiyong on 2019/7/23.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZYCoverControllerFramework.
+FOUNDATION_EXPORT double ZYCoverControllerFrameworkVersionNumber;
+
+//! Project version string for ZYCoverControllerFramework.
+FOUNDATION_EXPORT const unsigned char ZYCoverControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZYCoverControllerFramework/PublicHeader.h>
+
+
